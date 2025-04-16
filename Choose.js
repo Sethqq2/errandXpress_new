6ab3698d5ae28a3537919e719637fb3443b4032c
Sethqq2 <emmanuelakhidenor92@gmail.com>
2025-04-16 21:55:39 +0100

    Add files via upload

---
+++ Choose.js
@@ -0,0 +1,7 @@
+function sendSomeone() {
+    alert("You chose to send someone.");
+  }
+  
+  function goOnErrand() {
+    alert("You chose to go on an errand.");
+  }
---
+++ login.js
@@ -0,0 +1,9 @@
+// You can add JavaScript for form validation or other interactive features
+document.addEventListener('DOMContentLoaded', function() {
+    const loginButton = document.querySelector('.login-button');
+
+    loginButton.addEventListener('click', function() {
+        //  add your login logic here.
+        alert('Log In button clicked!');
+    });
+});
